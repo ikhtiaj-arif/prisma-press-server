b2087b9a5fb922721b96eb62c5ffda74a72720de
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-06-24 22:06:57 +0600

    catchAsync higher order function created to handle async req res

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import httpStatus from "http-status";
 import { userServices } from "./user.service";
+import { catchAsync } from "./utils/catchAsync";
 
-const userRegister = async (req: Request, res: Response) => {
-  console.log("controller");
-  try {
+
+
+const userRegister = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
     const result = await userServices.registerUserIntoDB(payload);
@@ -17,16 +19,9 @@ const userRegister = async (req: Request, res: Response) => {
         user: result,
       },
     });
-  } catch (error) {
-    console.log(error);
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to register user",
-      error: (error as Error).message,
-    });
-  }
-};
+  },
+);
+
 
 export const userController = {
   userRegister,
